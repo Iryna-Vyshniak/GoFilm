e47e17b930b74379f7b2cf8a66a350ed947138c6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-17 17:47:55 +0300

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import { ThemeProvider } from 'styled-components';
@@ -6,8 +7,10 @@ import { theme } from './components/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
