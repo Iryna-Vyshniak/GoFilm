f4416cdb4da297a491c03691e92418dc28e4cb04
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-19 01:15:33 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
-import { HomeBlock } from './Home.styled';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  HomeBlock,
+  MovieListItem,
+  MovieListStyled,
+  MoviePoster,
+  MovieTitle,
+  ReleaseDate,
+  Vote,
+} from './Home.styled';
+import { Title } from 'components/Title/Title';
+import { getPopularMovies } from 'services/themoviedbAPI';
 
 const Home = () => {
-  return <HomeBlock>Home</HomeBlock>;
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  //   const [totalResult, setTotalResult] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getPopularMovies(page);
+        console.log(data);
+        setMovies(data);
+      } catch (error) {
+        alert(error.message);
+      }
+    })();
+  }, [page]);
+
+  return (
+    <HomeBlock>
+      <Title title="Trending Today" />
+      {movies?.results && (
+        <MovieListStyled>
+          {movies.results.map(movie => (
+            <MovieListItem key={movie.id}>
+              <Link to={`/trending/movie/day`}>
+                <MoviePoster>
+                  <img
+                    src={
+                      movie.poster_path
+                        ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+                        : `https://astoriamuseums.org/wp-content/uploads/2020/10/OFM-poster-not-available.png`
+                    }
+                    alt={movie.title}
+                    width="200"
+                  />
+                </MoviePoster>
+                <MovieTitle>{movie.title}</MovieTitle>
+                <ReleaseDate>
+                  {parseInt(movie.release_date || movie.first_air_date)}
+                </ReleaseDate>
+                {movie.vote_average !== 0 && (
+                  <Vote>{movie.vote_average.toFixed(1)}</Vote>
+                )}
+              </Link>
+            </MovieListItem>
+          ))}
+        </MovieListStyled>
+      )}
+    </HomeBlock>
+  );
 };
 
 export default Home;
