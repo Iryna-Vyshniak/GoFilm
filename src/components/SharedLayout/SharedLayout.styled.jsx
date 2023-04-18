c8d4bb0d71bac08a926f858400355b0e1dff0753
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-18 22:17:48 +0300

    Create SharedLayout.styled.jsx

---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Container = styled.div`
+  max-width: 100%;
+  /* width: 1200px; */
+  margin: 0 auto;
+  /*  padding-left: ${({ theme }) => theme.space[4]}px;
+  padding-right: ${({ theme }) => theme.space[4]}px; */
+`;
+
+export const Header = styled.header`
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1100;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding-top: ${({ theme }) => theme.space[3]}px;
+  padding-bottom: ${({ theme }) => theme.space[3]}px;
+  padding-left: ${({ theme }) => theme.space[4]}px;
+  padding-right: ${({ theme }) => theme.space[4]}px;
+  margin-bottom: 16px;
+
+  width: 100%;
+
+  background-color: ${props => props.theme.colors.secondary};
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+
+  backdrop-filter: blur(3.5px);
+
+  > nav {
+    display: flex;
+  }
+`;
+
+export const Logo = styled.p`
+  font-weight: 700;
+  margin: 0;
+
+  svg {
+    fill: ${props => props.theme.colors.accent};
+    stroke: ${props => props.theme.colors.white};
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+    margin-right: 10px;
+    :hover {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+export const Link = styled(NavLink)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+
+  &.active {
+    color: white;
+    background-color: orangered;
+  }
+`;
