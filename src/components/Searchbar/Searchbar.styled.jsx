1175691e74b0399f761a46402a72681d82b35730
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-18 22:42:24 +0300

    update searchbar styles

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -1,30 +1,5 @@
 import styled from 'styled-components';
 
-export const Header = styled.header`
-  position: sticky;
-  top: 0;
-  left: 0;
-  z-index: 1100;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  min-height: 64px;
-
-  padding-right: ${props => props.theme.space[4]}px;
-  padding-left: ${props => props.theme.space[4]}px;
-  padding-top: ${props => props.theme.space[3]}px;
-  padding-bottom: ${props => props.theme.space[3]}px;
-
-  color: ${props => props.theme.colors.white};
-  background-color: ${props => props.theme.colors.secondary};
-  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
-
-  backdrop-filter: blur(3.5px);
-`;
-
 export const SearchForm = styled.form`
   display: flex;
   align-items: center;
