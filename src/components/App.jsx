7951203afc691d8f7a7dd921c355e44babf3f6a8
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-18 19:37:03 +0300

    add start test nav and Route path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+//import { lazy } from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+//import { Layout } from './Layout/Layout';
+// import Home from 'path/to/pages/Home';
+// import About from 'path/to/pages/About';
+// import Products from 'path/to/pages/Products';
+// import NotFound from 'path/to/pages/NotFound';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movies" element={<div>Movies</div>} />
+        {/* <Route path="/movies/:movieId" element="" />
+        <Route path="/movies/:movieId/cast" element="" />
+        <Route path="/movies/:movieId/reviews " element="" /> */}
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </div>
+  );
 };
