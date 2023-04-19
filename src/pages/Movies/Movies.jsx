d861f6e5197d13e3c17628fdeff6cb9577091269
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-19 18:47:11 +0300

    add InitialStateGallery and  test Link

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MoviesBlock } from './Movies.styled';
 import { Searchbar } from 'components/Searchbar/Searchbar';
+import { Link } from 'react-router-dom';
+import { InitialStateGallery } from 'components/InitialStateGallery/InitialStateGallery';
 
 const Movies = () => {
   // useEffect(() => {
@@ -9,6 +11,15 @@ const Movies = () => {
   return (
     <MoviesBlock>
       <Searchbar />
+      {/* стартове дефолтне зображення в галереї до рендеру фільмів */}
+      <InitialStateGallery text="Let`s find movies together!" />
+      <div>
+        {['film-1', 'film-2', 'film-3', 'film-4', 'film-5'].map(film => (
+          <Link key={film} to={`${film}`}>
+            {film}
+          </Link>
+        ))}
+      </div>
     </MoviesBlock>
   );
 };
