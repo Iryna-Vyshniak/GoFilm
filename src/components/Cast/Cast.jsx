54a04dfa1242f3ce37c32ad1c11f2803ab530727
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-19 23:08:09 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-export const Cast = () => {
+const Cast = () => {
   //const params = useParams();
   const { movieId } = useParams();
   console.log(movieId);
 
   return <div>Cast</div>;
 };
+
+export default Cast;
