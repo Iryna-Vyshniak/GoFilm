c3aee70ebb5d100a9ad7e4e62ee6f92f5ebf57c3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-19 18:01:54 +0300

    Update InitialStateGallery.jsx

--- src/components/InitialStateGallery/InitialStateGallery.jsx
+++ src/components/InitialStateGallery/InitialStateGallery.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Hero, Text, Wrapper } from './InitialStateGallery.styled';
+import Movie from 'assets/movie.png';
 
 export const InitialStateGallery = ({ text }) => {
   return (
     <Wrapper>
       <Text>{text}</Text>
-      <Hero src="" alt="" />
+      <Hero src={Movie} alt="Movie`s attributes" />
     </Wrapper>
   );
 };
