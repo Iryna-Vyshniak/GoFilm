d3f822d62c77e4a5b8c7e71251081ac18ab80270
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-19 18:49:51 +0300

    add Route for cast and reviews

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { SharedLayout } from './SharedLayout/SharedLayout';
 
 import Home from 'pages/Home/Home';
 import Movies from 'pages/Movies/Movies';
+import { MovieDetails } from 'pages/MovieDetails/MovieDetails';
 // const Home = lazy(() => import('pages/Home/Home'));
 // const Movies = lazy(() => import('pages/Movies/Movies'));
 
@@ -15,18 +16,16 @@ export const App = () => {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="/movies" element={<Movies />} />
-          <Route
-            path="/movies/:movieId"
-            element={<div>Element Movies ID</div>}
-          />
-          <Route
-            path="/movies/:movieId/cast"
-            element={<div>Element Movies ID Cast</div>}
-          />
-          <Route
-            path="/movies/:movieId/reviews "
-            element={<div>Element Movies ID Reviews</div>}
-          />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route
+              path="/movies/:movieId/cast"
+              element={<div>Element Movies ID Cast</div>}
+            />
+            <Route
+              path="/movies/:movieId/reviews"
+              element={<div>Element Movies ID Reviews</div>}
+            />
+          </Route>
           <Route path="*" element={<div>NotFound</div>} />
         </Route>
       </Routes>
