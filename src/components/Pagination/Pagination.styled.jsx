e26438997bf1c2e5766aa855f3e9c5076a5033a1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-20 10:37:55 +0300

    Create Pagination.styled.jsx

---
+++ src/components/Pagination/Pagination.styled.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import ReactPaginate from 'react-paginate';
+
+export const Paginate = styled(ReactPaginate)`
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+  margin: 10px 0;
+  font-size: 18px;
+
+  .selected {
+    border: 1px solid #ffffff;
+    background: #ecf0f3;
+    box-shadow: inset -3px -3px 7px #ffffff, inset 3px 3px 5px #ceced1;
+    text-shadow: 0.0625rem 0.0625rem 0.0625rem #fff;
+    color: ${props => props.theme.colors.accent};
+  }
+
+  a {
+    display: inline-block;
+    padding: 3px 0 2px 0;
+    vertical-align: middle;
+    width: 38px;
+    height: 32px;
+    text-align: center;
+
+    border-radius: 4px;
+    border: none;
+    outline: none;
+
+    box-shadow: -3px -3px 7px #ffffff73, 3px 3px 5px rgba(94, 104, 121, 0.288);
+
+    text-shadow: 0 1px 1px #fff;
+    background-color: transparent;
+
+    cursor: pointer;
+
+    &:hover {
+      background: #ecf0f3;
+      box-shadow: inset -3px -3px 7px #ffffff, inset 3px 3px 5px #ceced1;
+      color: ${props => props.theme.colors.accent};
+    }
+  }
+`;
