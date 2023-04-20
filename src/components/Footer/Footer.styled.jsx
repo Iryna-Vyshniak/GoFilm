471dac14099eedb677be64e4ac2e9e76e1d91565
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-21 00:43:19 +0300

    Update Footer.styled.jsx

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -4,7 +4,7 @@ export const FooterBlock = styled.footer`
   position: relative;
   display: flex;
   padding: 16px 0;
-  background-color: rgba(185, 228, 201, 0.4);
+  background: #b9e4c9;
   box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2),
     0 4px 5px 1px rgba(0, 0, 0, 0.14), 0px 1px 10px 1px rgba(0, 0, 0, 0.12);
 `;
@@ -90,3 +90,21 @@ export const SocialIcons = styled.div`
 color: ${props => props.theme.colors.accent};
 }
 }`;
+
+export const Btn = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  outline: none;
+  width: 34px;
+  height: 34px;
+  border-radius: 50%;
+  background: transparent;
+  box-shadow: -3px -3px 7px #ffffff, 3px 3px 7px rgba(185, 228, 201, 0.773);
+
+  :hover {
+    background: #ecf0f3;
+    box-shadow: inset -3px -3px 7px #ffffff, inset 3px 3px 5px #ceced1;
+  }
+`;
