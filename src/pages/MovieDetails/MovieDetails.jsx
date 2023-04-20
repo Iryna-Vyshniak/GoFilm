3747c4f5059d15306a79db55663f9fd455e5a688
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-21 02:59:50 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,25 +1,29 @@
 import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { Outlet, useLocation, useParams } from 'react-router-dom';
 import dayjs from 'dayjs';
+import ReactPlayer from 'react-player';
 import { LinkToBack } from 'components/LinkToBack/LinkToBack';
 import { FaYoutube } from 'react-icons/fa';
 
 import {
   AccentText,
+  BackdropImg,
   Btn,
   MainTitle,
   MovieInfo,
+  MovieList,
   NavLink,
-  PlayBox,
+  PlayVideoBox,
+  RatingProgressbar,
+  WrapperDetails,
   WrapperMovie,
 } from './MovieDetails.styled';
 
 import { getMoviesDetailsById, getVideosMovies } from 'services/themoviedbAPI';
 import { Loader } from 'components/Loader/Loader';
 import ImageErrorView from 'components/ImageErrorView/ImageErrorView';
-import { RatingProgressbar } from 'components/RatingProgressbar/RatingProgressbar';
+// import { RatingProgressbar } from 'components/RatingProgressbar/RatingProgressbar';
 import { Modal } from 'components/Modal/Modal';
-import ReactPlayer from 'react-player';
 
 const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState([]);
@@ -65,6 +69,7 @@ const MovieDetails = () => {
             movie.name.includes('Official') ||
             movie.name[0]
         );
+        console.log(key);
         setMovieVideo(key.key);
       } catch (error) {
         console.log(error.message);
@@ -96,109 +101,150 @@ const MovieDetails = () => {
     overview,
     status,
     runtime,
+    production_companies,
+    backdrop_path,
   } = movieDetails;
 
   return (
     <>
       {isLoading && <Loader />}
       {error && <ImageErrorView message="Oops, mistake! Please try again" />}
-      <WrapperMovie>
-        {`https://image.tmdb.org/t/p/w500${poster_path}` && (
-          <img
-            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-            alt=""
-            width="300"
-          />
-        )}
-        <div>
-          <MainTitle>
-            {original_title || original_name}{' '}
-            {release_date && <span> ({parseInt(release_date)})</span>}
-          </MainTitle>
 
-          <div style={{ position: 'relative' }}>
-            {vote_average && vote_average !== 0 ? (
-              <RatingProgressbar
-                style={{ bottom: '0', width: '44px', height: '44px' }}
-                rating={vote_average.toFixed(1)}
+      {!error && (
+        <>
+          <>
+            <BackdropImg>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
+                alt="poster opacity"
+                width="100"
               />
-            ) : (
-              ''
+            </BackdropImg>
+          </>
+          <WrapperMovie>
+            {`https://image.tmdb.org/t/p/w500${poster_path}` && (
+              <img
+                src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                alt=""
+                width="300"
+              />
             )}
+            <WrapperDetails>
+              <MainTitle>
+                {original_title || original_name}{' '}
+                {release_date && <span> ({parseInt(release_date)})</span>}
+              </MainTitle>
 
-            {movieVideo.length > 0 && (
-              <PlayBox>
-                <Btn onClick={toggleModal}>
-                  <FaYoutube
-                    size="34px"
-                    color="red"
-                    style={{ marginRight: '10px' }}
+              <div style={{ position: 'relative' }}>
+                {vote_average && vote_average !== 0 ? (
+                  <RatingProgressbar
+                    style={{
+                      position: 'absolute',
+                      bottom: '0',
+                      width: '44px',
+                      height: '44px',
+                    }}
+                    rating={vote_average.toFixed(1)}
                   />
-                </Btn>
-                <span>Watch Trailer</span>
-              </PlayBox>
-            )}
-          </div>
-          <h2>Overview</h2>
-          <p>{overview}</p>
-          <h2>Genres</h2>
-          <p>
-            {genres &&
-              genres.map((genre, index) => (
-                <span key={index} style={{ marginRight: '10px' }}>
-                  {genre.name} /
-                </span>
-              ))}
-          </p>
-          {status && (
-            <div>
-              <AccentText>Status: </AccentText>
-              <span>{status}</span>
-            </div>
-          )}
-          {release_date && (
-            <div>
-              <AccentText>Release Date: </AccentText>
-              <span>{dayjs(release_date).format('MMM D, YYYY')}</span>
-            </div>
-          )}
-          {runtime !== 0 && (
-            <div>
-              <AccentText>Runtime: </AccentText>
-              <span>{toHoursAndMinutes(runtime)}</span>
-            </div>
-          )}
-        </div>
-      </WrapperMovie>
-      <MovieInfo>
-        <h1>Additional information</h1>
-        <LinkToBack
-          to={backLinkLocationRef.current}
-          children="Back to Movies"
-        />
-        <nav>
-          <NavLink to="cast" state={location.state}>
-            Cast
-          </NavLink>
-          <NavLink to="reviews" state={location.state}>
-            Reviews
-          </NavLink>
-        </nav>
-      </MovieInfo>
+                ) : (
+                  ''
+                )}
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Outlet />
-      </Suspense>
-      {showModal && (
-        <Modal closeModal={toggleModal}>
-          <ReactPlayer
-            muted={true}
-            controls={true}
-            width="100%"
-            height="100%"
-            url={`https://www.youtube.com/watch?v=${movieVideo}`}
-          />
-        </Modal>
+                {movieVideo.length > 0 && (
+                  <PlayVideoBox>
+                    <Btn onClick={toggleModal}>
+                      <FaYoutube
+                        size="64px"
+                        color="red"
+                        style={{ marginRight: '10px' }}
+                      />
+                    </Btn>
+                    {/* <span>Watch Trailer</span> */}
+                  </PlayVideoBox>
+                )}
+              </div>
+              <h2>Overview</h2>
+              <p>{overview}</p>
+              <h2>Genres</h2>
+              <p>
+                {genres &&
+                  genres.map((genre, index) => (
+                    <span key={index} style={{ marginRight: '10px' }}>
+                      {genre.name} /
+                    </span>
+                  ))}
+              </p>
+              {production_companies && (
+                <>
+                  <h2>Production Companies</h2>
+                  <MovieList>
+                    {production_companies.map(
+                      (production, idx) =>
+                        production.logo_path !== null && (
+                          <li key={idx}>
+                            <img
+                              src={`https://image.tmdb.org/t/p/w500${production.logo_path}`}
+                              alt="company logo"
+                              width="80"
+                            />
+                          </li>
+                        )
+                    )}
+                  </MovieList>
+                </>
+              )}
+              {status && (
+                <div>
+                  <AccentText>Status: </AccentText>
+                  <span>{status}</span>
+                </div>
+              )}
+              {release_date && (
+                <div>
+                  <AccentText>Release Date: </AccentText>
+                  <span>{dayjs(release_date).format('MMM D, YYYY')}</span>
+                </div>
+              )}
+              {runtime !== 0 && (
+                <div>
+                  <AccentText>Runtime: </AccentText>
+                  <span>{toHoursAndMinutes(runtime)}</span>
+                </div>
+              )}
+            </WrapperDetails>
+          </WrapperMovie>
+          <MovieInfo>
+            <h1>Additional information</h1>
+            <LinkToBack
+              to={backLinkLocationRef.current}
+              children="Back to Movies"
+            />
+            <nav>
+              <NavLink to="cast" state={location.state}>
+                Cast
+              </NavLink>
+              <NavLink to="reviews" state={location.state}>
+                Reviews
+              </NavLink>
+            </nav>
+          </MovieInfo>
+
+          <Suspense fallback={<div>Loading...</div>}>
+            <Outlet />
+          </Suspense>
+          {showModal && (
+            <Modal closeModal={toggleModal}>
+              <ReactPlayer
+                muted={true}
+                controls={true}
+                width="100%"
+                height="100%"
+                url={`https://www.youtube.com/watch?v=${movieVideo}`}
+                //autoPlay={true}
+              />
+            </Modal>
+          )}
+        </>
       )}
     </>
   );
