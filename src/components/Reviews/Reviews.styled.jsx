6cd4dcf58309cae1d6c8b37ecab62cc1c4001091
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-20 10:38:04 +0300

    Update Reviews.styled.jsx

--- src/components/Reviews/Reviews.styled.jsx
+++ src/components/Reviews/Reviews.styled.jsx
@@ -1,1 +1,44 @@
 import styled from 'styled-components';
+
+export const ReviewsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+export const ReviewItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  font-size: 12px;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 8px;
+`;
+export const ReviewPhotoThumb = styled.div`
+  position: relative;
+`;
+
+export const ReviewPhoto = styled.img`
+  display: block;
+
+  height: 60px;
+  width: auto;
+
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+
+  object-fit: cover;
+  object-position: center;
+`;
+
+export const ReviewsAuthor = styled.p`
+  color: ${props => props.theme.colors.accent};
+  font-weight: 700;
+`;
+export const ReviewContent = styled.p`
+  font-style: italic;
+  font-weight: 400;
+`;
