5530edc0addee33dd759f9bcfffabb0e04e179e1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-20 10:49:27 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  HomeBlock,
-  MovieListItem,
-  MovieListStyled,
-  MoviePoster,
-  MovieTitle,
-  ReleaseDate,
-} from './Home.styled';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { HomeBlock } from './Home.styled';
 import { Title } from 'components/Title/Title';
+import Pagination from 'components/Pagination/Pagination';
+import { MovieGallery } from 'components/MovieGallery/MovieGallery';
+import { Loader } from 'components/Loader/Loader';
+import ImageErrorView from 'components/ImageErrorView/ImageErrorView';
 import { getPopularMovies } from 'services/themoviedbAPI';
-import { RatingProgressbar } from 'components/RatingProgressbar/RatingProgressbar';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
-  const [page /*  setPage */] = useState(1);
-  //   const [totalResult, setTotalResult] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const page = Number(searchParams.get('page'));
+  //console.log(page);
 
   useEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
+        setError(false);
+
         const data = await getPopularMovies(page);
-        console.log(data);
-        setMovies(data);
+        // console.log(data);
+        setMovies(data.results);
+        setTotalPages(data.total_pages);
       } catch (error) {
         console.log(error.message);
+      } finally {
+        setIsLoading(false);
       }
     })();
   }, [page]);
@@ -33,36 +40,21 @@ const Home = () => {
     return <div>Loading...</div>;
   }
 
+  const handlePageClick = e => {
+    //console.log(e.selected);
+    setSearchParams({ page: e.selected + 1 });
+  };
+
   return (
     <HomeBlock>
       <Title title="Trending Today" />
-      {movies?.results && (
-        <MovieListStyled>
-          {movies.results.map(movie => (
-            <MovieListItem key={movie.id}>
-              <Link to={`/trending/movie/day`}>
-                <MoviePoster>
-                  <img
-                    src={
-                      movie.poster_path
-                        ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
-                        : `https://astoriamuseums.org/wp-content/uploads/2020/10/OFM-poster-not-available.png`
-                    }
-                    alt={movie.title}
-                    width="200"
-                  />
-                </MoviePoster>
-                <MovieTitle>{movie.title}</MovieTitle>
-                <ReleaseDate>
-                  {parseInt(movie.release_date || movie.first_air_date)}
-                </ReleaseDate>
-                {movie.vote_average !== 0 && (
-                  <RatingProgressbar rating={movie.vote_average.toFixed(1)} />
-                )}
-              </Link>
-            </MovieListItem>
-          ))}
-        </MovieListStyled>
+      {isLoading && <Loader />}
+      {error && <ImageErrorView message="Oops, mistake! Please try again" />}
+      {!error && (
+        <>
+          <MovieGallery movies={movies} />
+          <Pagination handlePageClick={handlePageClick} pages={totalPages} />
+        </>
       )}
     </HomeBlock>
   );
