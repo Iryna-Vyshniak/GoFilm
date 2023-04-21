b13e3624f441137d7b9e81e32b87789d9c87e8e1
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-22 01:40:05 +0300

    Create PageNotFound.jsx

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { PageNf, Wrap } from './PageNotFound.styled';
+
+export const PageNotFound = () => {
+  return (
+    <div>
+      <PageNf>
+        <Wrap>
+          <h1>404</h1>
+          <p>Page not found...</p>
+        </Wrap>
+      </PageNf>
+    </div>
+  );
+};
