17ef8bdd17190c547481344e4725d77f56d9c5fa
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-22 03:58:57 +0300

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -1,35 +1,17 @@
 import styled from 'styled-components';
 
-export const ModalBackdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1200;
-
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.7);
-`;
-
-export const ModalContent = styled.div`
+export const CloseBtn = styled.button`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  top: 5px;
+  right: 5px;
 
-  width: 100%;
-  height: calc(100vw / 16 * 9);
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: 1px solid #fd5523;
 
-  @media screen and (min-width: 768px) {
-    max-width: 1067px;
-    max-height: 600px;
-  }
-
-  border-radius: 3px;
-  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-
-  pointer-events: none;
-  opacity: 1;
-  transition: opacity 300ms, transform 300ms;
+  background: #ecf0f3;
+  box-shadow: inset -3px -3px 7px #ffffff, inset 3px 3px 5px #ceced1,
+    2px 2px 2px #0f0f0f;
+  color: #fd5523;
 `;
