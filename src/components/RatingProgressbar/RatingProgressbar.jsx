051c44a69ef8a1fd0bb4df24e6f9cc2ffae5acb4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-23 00:03:23 +0300

    Update RatingProgressbar.jsx

--- src/components/RatingProgressbar/RatingProgressbar.jsx
+++ src/components/RatingProgressbar/RatingProgressbar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
-// import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-// import { CircleRating } from './RatingProgressbar.styled';
+
 
 export const RatingProgressbar = ({ rating }) => {
   return (
@@ -12,56 +11,22 @@ export const RatingProgressbar = ({ rating }) => {
       text={rating}
       background
       backgroundPadding={6}
-      /*  styles={buildStyles({
-          pathColor: rating < 5 ? 'red' : rating < 7 ? 'orangered' : 'green',
-          position: 'absolute',
-          height: '100%',
-          width: '100%',
-          textSize: '44px',
-          fontWeight: '700',
-          strokeLinecap: 'butt',
-          transition: 'stroke-dashoffset 0.5s ease 0s',
-          transform: 'rotate(0.25turn)',
-          transformOrigin: 'center center',
-          pathTransitionDuration: 0.5,
-          textColor: '#fd5523',
-          trailColor: '#d6d6d6',
-          backgroundColor: 'rgba(185, 228, 201, 0.773)',
-        })} */
-      styles={{
-        // Customize the root svg element
-        root: {
-          position: 'absolute',
-          bottom: '18px',
-          left: '10px',
-          width: '44px',
-          height: '44px',
-        },
-        // Customize the path, i.e. the "completed progress"
-        path: {
-          stroke: rating < 5 ? 'red' : rating < 7 ? 'orangered' : 'green',
-          strokeLinecap: 'butt',
-          transition: 'stroke-dashoffset 0.5s ease 0s',
-          transform: 'rotate(2turn)',
-          transformOrigin: 'center center',
-        },
-        // Customize the circle behind the path, i.e. the "total progress"
-        trail: {
-          stroke: '#fff',
-          strokeLinecap: 'butt',
-          transform: 'rotate(2.5turn)',
-          transformOrigin: 'center center',
-        },
-        // Customize the text
-        text: {
-          fill: '#fd5523',
-          fontSize: '44px',
-        },
-        // Customize background - only used when the `background` prop is true
-        background: {
-          fill: 'rgba(185, 228, 201, 0.773)',
-        },
-      }}
+      styles={buildStyles({
+        pathColor: rating < 5 ? 'red' : rating < 7 ? 'orangered' : 'green',
+        height: '100%',
+        width: '100%',
+        textSize: '44px',
+        fontWeight: '700',
+        strokeLinecap: 'butt',
+        transition: 'stroke-dashoffset 0.5s ease 0s',
+        transform: 'rotate(0.25turn)',
+        transformOrigin: 'center center',
+        pathTransitionDuration: 0.5,
+        textColor: '#fd5523',
+        trailColor: '#fff',
+        backgroundColor: 'rgba(185, 228, 201, 0.773)',
+      })}
     />
   );
 };
+
