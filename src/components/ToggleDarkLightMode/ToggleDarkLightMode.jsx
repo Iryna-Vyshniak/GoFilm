dc61def2d60ff5cd0395e488ff0b78eccd4b408c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-23 00:03:56 +0300

    Update ToggleDarkLightMode.jsx

--- src/components/ToggleDarkLightMode/ToggleDarkLightMode.jsx
+++ src/components/ToggleDarkLightMode/ToggleDarkLightMode.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { func, string } from 'prop-types';
-
 import { ReactComponent as MoonIcon } from 'assets/moon.svg';
 import { ReactComponent as SunIcon } from 'assets/sun.svg';
 import { ToggleContainer } from './ToggleDarkLightMode.styled';
