4c5b0fa901526cc51d60b252e5531630053c20da
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-23 22:54:37 +0300

    Update Movies.styled.jsx

--- src/pages/Movies/Movies.styled.jsx
+++ src/pages/Movies/Movies.styled.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const MoviesBlock = styled.div`
+  position: relative;
+
   display: flex;
   flex-direction: column;
   gap: 24px;
@@ -12,7 +14,7 @@ export const MoviesBlock = styled.div`
   text-align: center;
 
   @media screen and (min-width: 480px) {
-    font-size: ${({ theme }) => theme.fontSizes.l};
+    font-size: ${({ theme }) => theme.fontSizes.xl};
   }
 `;
 
@@ -53,4 +55,73 @@ export const Poster = styled.img`
   border-radius: 8px;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+`;
+
+//--------------------------------------------------
+export const BackdropImg = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.5;
+  overflow: hidden;
+
+  flex: 1 1 auto;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top;
+  }
+`;
+
+export const GradientBlockTop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background: ${({ theme }) => theme.gradientBlock};
+`;
+
+export const GradientBlockBottom = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 450px;
+
+  background: ${({ theme }) => theme.gradientHeroBanner};
+`;
+
+export const SectionHero = styled.section`
+  position: relative;
+
+  margin-top: -50px;
+  margin-bottom: 30px;
+
+  padding-top: 84px;
+  padding-bottom: 84px;
+
+  background-color: rgba(0, 0, 0, 1);
+`;
+export const HeroContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0 auto;
+  padding: 0;
+
+  width: 100%;
+  height: 450px;
+
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
 `;
