0b34a22e323d290ff957f849494ccc973a806be7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-23 17:32:24 +0300

    Update Movies.styled.jsx

--- src/pages/Movies/Movies.styled.jsx
+++ src/pages/Movies/Movies.styled.jsx
@@ -47,3 +47,10 @@ export const Btn = styled.button`
     transform: scale(0.8);
   }
 `;
+
+export const Poster = styled.img`
+  display: inline-block;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+`;
