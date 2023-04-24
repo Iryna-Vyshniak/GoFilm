a6e8d342c67903a4da97a40450f1d5b024ed8a18
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-24 15:59:22 +0300

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -34,16 +34,23 @@ export const ModalDescr = styled.p`
 `;
 
 export const BtnScrollToTop = styled(ScrollToTop)`
-  display: flex;
+  /* display: flex;
   justify-content: center;
   align-items: center;
-  width: 42px;
+ */
+
   padding: 8px;
+  width: 44px;
   background-color: rgba(185, 228, 201, 0.5);
-  border-radius: 8px;
+
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   transform: scale(1);
+
+  :active {
+    background-color: #ecf0f3;
+    box-shadow: inset -3px -3px 7px #ffffff, inset 3px 3px 5px #ceced1;
+  }
 `;
