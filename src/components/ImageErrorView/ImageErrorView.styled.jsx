d0dc61a34213af87836bc10075138b5bcbe88f74
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-24 15:59:58 +0300

    Update ImageErrorView.styled.jsx

--- src/components/ImageErrorView/ImageErrorView.styled.jsx
+++ src/components/ImageErrorView/ImageErrorView.styled.jsx
@@ -20,10 +20,67 @@ export const Text = styled.p`
   font-size: ${props => props.theme.fontSizes.xl}
 `;
 
-export const ErrorImg = styled.img`
-  width: 25vw;
+export const Hero = styled.img`
+  width: 50vw;
   height: auto;
   object-fit: contain;
   object-position: center;
-  margin-bottom: 24px;
+  margin: 0 auto;
+`;
+
+export const BackdropImg = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  overflow: hidden;
+  flex: 1 0 auto;
+
+  img {
+    opacity: 0.8;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: top;
+  }
+`;
+
+export const GradientBlockTop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background: ${({ theme }) => theme.gradientBlock};
+`;
+
+export const GradientBlockBottom = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 45vw;
+
+  background: ${({ theme }) => theme.gradientHeroBanner};
+`;
+
+export const BlockHero = styled.div`
+  position: relative;
+  margin-top: -30px;
+  margin-bottom: 30px;
+`;
+export const HeroContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0;
+  width: 100%;
+
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
 `;
