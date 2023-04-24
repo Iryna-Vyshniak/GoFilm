eecafb422dbf91508611d6aa41fed8b4aa92051e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-24 16:00:32 +0300

    add block hero

--- src/components/ImageErrorView/ImageErrorView.jsx
+++ src/components/ImageErrorView/ImageErrorView.jsx
@@ -1,11 +1,26 @@
-import { ErrorImg, Text, Wrapper } from './ImageErrorView.styled';
+import HeroImg from 'assets/error.png';
+import {
+  BlockHero,
+  GradientBlockBottom,
+  GradientBlockTop,
+  Hero,
+  HeroContainer,
+  Text,
+  Wrapper,
+} from './ImageErrorView.styled';
 import PropTypes from 'prop-types';
 
 export default function ImageErrorView({ message }) {
   return (
     <Wrapper role="alert">
-      <ErrorImg src="" width="240" alt="" />
       <Text>{message}</Text>
+      <BlockHero>
+        <HeroContainer>
+          <GradientBlockTop></GradientBlockTop>
+          <Hero src={HeroImg} alt="Actor" width="240" />
+          <GradientBlockBottom></GradientBlockBottom>
+        </HeroContainer>
+      </BlockHero>
     </Wrapper>
   );
 }
