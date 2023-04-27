055f72d2b0ee2d496ab1aaba611b70a25360e23d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-27 17:27:02 +0300

    Update MovieGalleryItem.jsx

--- src/components/MovieGalleryItem/MovieGalleryItem.jsx
+++ src/components/MovieGalleryItem/MovieGalleryItem.jsx
@@ -5,9 +5,11 @@ import {
   MoviePoster,
   MovieTitle,
   ReleaseDate,
+  Stars,
 } from './MovieGalleryItem.styled';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Rating } from '@mui/material';
 
 import { RatingProgressbar } from 'components/RatingProgressbar/RatingProgressbar';
 import NoPoster from 'assets/no-poster.jpg';
@@ -33,6 +35,22 @@ export const MovieGalleryItem = ({
         </MoviePoster>
         <MovieTitle>{original_title}</MovieTitle>
         {release_date && <ReleaseDate>{parseInt(release_date)}</ReleaseDate>}
+        {vote_average !== 0 && (
+          <Stars>
+            <Rating
+              style={{
+                position: 'absolute',
+                top: '0',
+                right: '0',
+              }}
+              name="read-only"
+              defaultValue={(vote_average / 10) * 5}
+              precision={0.5}
+              size="small"
+              readOnly
+            />
+          </Stars>
+        )}
         {vote_average !== 0 && (
           <CircleRating>
             <RatingProgressbar rating={vote_average.toFixed(1)} />
