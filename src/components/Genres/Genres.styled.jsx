fe216eadeeae762ee5f82b61ad0ff3a255103633
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-27 21:25:36 +0300

    Create Genres.styled.jsx

---
+++ src/components/Genres/Genres.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const GenresBlock = styled.ul`
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+`;
+export const Genre = styled.li`
+  background-color: rgba(185, 228, 201, 0.873);
+  color: rgb(0, 0, 0);
+  font-size: 12px;
+  font-weight: bold;
+  border-radius: 5px;
+  padding: 0 6px;
+`;
