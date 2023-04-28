391db2cba55366fc55db508cee5b165011a0c33e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-28 15:05:15 +0300

    Update Genres.styled.jsx

--- src/components/Genres/Genres.styled.jsx
+++ src/components/Genres/Genres.styled.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 export const GenresBlock = styled.ul`
-
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 8px;
 `;
 export const Genre = styled.li`
-  background-color: rgba(185, 228, 201, 0.873);
+  box-shadow: 1px 1px 2px #fff, 1px 1px 2px #fff;
+  background-color: rgba(185, 228, 201, 0.973);
   color: rgb(0, 0, 0);
+  text-shadow: 0 0.5px 0.5px #fff;
   font-size: 12px;
   font-weight: bold;
   border-radius: 5px;
