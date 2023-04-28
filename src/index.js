a11d70d26f11979862938e2d8636895f432e9f8a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-28 23:58:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
+import 'services/i18next';
+
 import { App } from './components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './components/theme';
 
+const loadingMarkup = (
+  <div style={{ textAlign: 'center' }}>
+    <h3>Loading...</h3>
+  </div>
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <I18nextProvider>
+        <BrowserRouter basename="/goit-react-hw-05-movies">
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      </I18nextProvider>
+    </React.StrictMode>
+  </Suspense>
 );
