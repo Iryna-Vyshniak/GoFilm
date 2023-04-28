97470fd8bdb4cf4fab8b90651ca3619e992179ea
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-28 23:57:31 +0300

    Update useLocaleStorage.jsx

--- src/hooks/useLocaleStorage.jsx
+++ src/hooks/useLocaleStorage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export const useLocaleStorage = (key, defaultValue) => {
+export const useLocalStorage = (key, defaultValue) => {
   const [state, setState] = useState(() => {
     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
   });
