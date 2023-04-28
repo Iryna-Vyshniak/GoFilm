a7e982fddc8fbd815dc38001a2d9b151cb2961d3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-29 00:47:18 +0300

    update

---
+++ src/components/LangMenu/LangMenu.styled.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const TitleLang = styled.li`
+  width: 100%;
+  font-size: 12px;
+  color: #000;
+  text-shadow: 0 0.4px 0.4px #fff;
+  text-align: center;
+`;
+
+export const Btn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+  width: 100%;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  font-size: 10px;
+  color: #fd5523;
+  text-shadow: 0 0.4px 0.4px #fff;
+`;
