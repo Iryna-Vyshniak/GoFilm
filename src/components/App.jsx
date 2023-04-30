fb111afcd450ecbbb044030d064b354dd0ab8657
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-01 02:40:42 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useSearchParams } from 'react-router-dom';
 
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,6 +8,8 @@ import { GlobalStyle } from './GlobalStyle';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { PageNotFound } from './PageNotFound/PageNotFound';
 import ScrollToTop from 'react-scroll-up';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Home = lazy(() => import('pages/Home/Home'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
@@ -21,26 +23,18 @@ const Reviews = lazy(() =>
   }))
 );
 
-const languages = [
-  {
-    code: 'en',
-    name: 'English',
-    country_code: 'gb',
-  },
-  {
-    code: 'uk',
-    name: 'Українська',
-    country_code: 'ua',
-  },
-];
-
-export const App = () => {
+export const App = ({ languages }) => {
   const currentLanguageCode = window.localStorage.getItem('i18nextLng') || 'en';
-  console.log(currentLanguageCode);
-
-  const currentLanguage = languages.find(
+  const currentLanguage = languages?.find(
     lang => lang.code === currentLanguageCode
   );
+  const [searchParams] = useSearchParams({ language: currentLanguageCode });
+  const lng = searchParams.get('language') ?? currentLanguageCode;
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage(lng);
+  }, [lng, i18n]);
 
   return (
     <>
@@ -51,21 +45,23 @@ export const App = () => {
             <SharedLayout
               currentLanguage={currentLanguage}
               languages={languages}
+              lng={lng}
             />
           }
         >
-          <Route
-            index
-            element={<Home currentLanguage={currentLanguageCode} />}
-          />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          <Route path="/" element={<Home lng={lng} />} />
+          <Route path="/movies" element={<Movies lng={lng} />} />
+          <Route path="/movies/:movieId" element={<MovieDetails lng={lng} />}>
+            <Route path="/movies/:movieId/cast" element={<Cast lng={lng} />} />
+            <Route
+              path="/movies/:movieId/reviews"
+              element={<Reviews lng={lng} />}
+            />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
+
       <ScrollToTop
         showUnder={120}
         style={{
@@ -88,3 +84,59 @@ export const App = () => {
     </>
   );
 };
+
+// export const App = ({ languages }) => {
+//   const currentLanguageCode = window.localStorage.getItem('i18nextLng') || 'en';
+//   const currentLanguage = languages?.find(
+//     lang => lang.code === currentLanguageCode
+//   );
+//   const [searchParams] = useSearchParams({ language: currentLanguageCode });
+//   const lng = searchParams.get('language') ?? currentLanguageCode;
+//   const { i18n } = useTranslation();
+
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   useEffect(() => {
+//     i18n.changeLanguage(lng);
+//     // Отримання параметрів запиту з адресного рядка
+//     const params = new URLSearchParams(window.location.search);
+//     const query = params.get('query') || '';
+//     const page = parseInt(params.get('page'), 10) || 1;
+
+//     setSearchQuery(query);
+//     setCurrentPage(page);
+//   }, [lng, i18n]);
+
+//   return (
+//     <>
+//       <Routes basename={`${currentLanguage.code}`}>
+//         <Route
+//           path="/"
+//           element={
+//             <SharedLayout
+//               currentLanguage={currentLanguage}
+//               languages={languages}
+//               lng={lng}
+//             />
+//           }
+//         >
+//           <Route path="/" element={<Home lng={lng} />} />
+//           {/*  <Route path="/movies" element={<Movies lng={lng} />} /> */}
+//           <Route
+//             path="/movies"
+//             element={
+//               <Movies
+//                 searchQuery={searchQuery}
+//                 currentPage={currentPage}
+//                 lng={lng}
+//               />
+//             }
+//           />
+//           <Route path="/movies/:movieId" element={<MovieDetails />}>
+//             <Route path="/movies/:movieId/cast" element={<Cast />} />
+//             <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+//           </Route>
+//           <Route path="*" element={<PageNotFound />} />
+//         </Route>
+//       </Routes>
