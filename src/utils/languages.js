ec5b219551542c33c7d43349bf63f2cf74bfdb34
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-01 15:23:07 +0300

    Create languages.js

---
+++ src/utils/languages.js
@@ -0,0 +1,12 @@
+export const languages = [
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+  },
+  {
+    code: 'uk',
+    name: 'Українська',
+    country_code: 'ua',
+  },
+];
