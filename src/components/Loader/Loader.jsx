fda6cdd7fdc05f9a0707a0bc1d527bf8398f706f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-01 17:43:09 +0300

    change spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { LoaderBackdrop, PendingDog } from './Loader.styled';
-import DotLoader from 'react-spinners/DotLoader';
+import { LoaderBackdrop } from './Loader.styled';
+import RingLoader from 'react-spinners/RingLoader';
 
 export const Loader = () => {
   return (
     <LoaderBackdrop>
-      <DotLoader
+      <RingLoader
         color="#f68000"
         size={150}
         cssOverride={{ position: 'absolute', top: '25%', left: '45%' }}
       />
-      <PendingDog src='' alt='' />
     </LoaderBackdrop>
   );
 };
