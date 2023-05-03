d17bb8532c107289b1f96d59ac59dd458107d3ed
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-03 16:43:06 +0300

    Create ActorsSearch.jsx

---
+++ src/components/ActorsSearch/ActorsSearch.jsx
@@ -0,0 +1,32 @@
+import {
+  Icon,
+  SearchForm,
+  SearchFormBtn,
+  SearchFormInput,
+} from './ActorsSearch.styled';
+
+import PropTypes from 'prop-types';
+
+export const ActorsSearch = ({ value, onChange, t }) => {
+  return (
+    <SearchForm>
+      <SearchFormInput
+        type="text"
+        value={value}
+        debounceTimeout={500}
+        onChange={onChange}
+        autoFocus
+        placeholder="Search actors..."
+      />
+      <SearchFormBtn type="submit">
+        <Icon size="24" />
+      </SearchFormBtn>
+    </SearchForm>
+  );
+};
+
+ActorsSearch.propType = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  t: PropTypes.any.isRequired,
+};
