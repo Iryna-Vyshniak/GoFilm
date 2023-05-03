896d97bb82a4c0efa9cc3830bc0c92c96cf40208
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-03 16:43:20 +0300

    Create ScrollInfo.styled.jsx

---
+++ src/components/ScrollInfo/ScrollInfo.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const BtnReadMore = styled.button`
+  padding: 8px 16px;
+  border-radius: 4px;
+  color: ${props => props.theme.colors.accent};
+  text-shadow: 0 0.4px 0.4px #fff, inset 40px 40px 40px #fff;
+  font-size: ${props => props.theme.fontSizes.m};
+  font-weight: 500;
+  opacity: 0.5;
+
+  background-color: transparent;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  :hover {
+    opacity: 1;
+    color: #fff;
+    background-color: #fd5523;
+  }
+`;
