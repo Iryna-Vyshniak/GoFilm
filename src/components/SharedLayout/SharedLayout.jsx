16364383e7f361e1958a2056e9b43a0d75388ac4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-03 17:20:43 +0300

    add translation

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -50,7 +50,7 @@ export const SharedLayout = ({ currentLanguage, languages }) => {
             <nav>
               <Link to="/">{t('home_link')}</Link>
               <Link to="/movies">{t('movie_link')}</Link>
-              <Link to="/actors">Actors</Link>
+              <Link to="/actors">{t('actor_link')}</Link>
             </nav>
             <LangThemeBlock>
               <LangMenu t={t} languages={languages} />
