398aef7b279aafa82d78a66f717f8447d48c75c3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-05 18:12:00 +0300

    Update HomePage.styled.jsx

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
 export const HomeBlock = styled.div`
-  // padding-top: ${({ theme }) => theme.space[5]}px;
-  // padding-bottom: ${({ theme }) => theme.space[5]}px;
-
   padding-left: ${({ theme }) => theme.space[4]}px;
   padding-right: ${({ theme }) => theme.space[4]}px;
   font-size: ${({ theme }) => theme.fontSizes.s};
