1348cd8305c75382beb27c960d0aa6124db55526
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-05-06 13:48:09 +0300

    Update Title.styled.jsx

--- src/components/Title/Title.styled.jsx
+++ src/components/Title/Title.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const MainTitle = styled.h1`
   margin-bottom: ${props => props.theme.space[4]}px;
   color: ${({ theme }) => theme.text};
-  text-shadow: 0 0.4px 0.4px #fff, 0 0.4px 0.4px #4b1414;
+  text-shadow: 0 0.4px 0.4px #fff, 0 0.5px 0.5px #4b1414;
   font-size: ${({ theme }) => theme.fontSizes.m};
   font-family: ${props => props.theme.fonts.heading};
   font-weight: 700;
